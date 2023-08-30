46f801c1fb088787dd91d257eac9d28713571803
Caio Bianchi <caiohbs23@gmail.com>
2023-08-30 11:54:18 -0300

    nodata final version

--- nodata/entrypoint.sh
+++ nodata/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# python3 manage.py migrate --no-input
-# python3 manage.py collectstatic --no-input
+python3 manage.py migrate --no-input
+python3 manage.py collectstatic --no-input
+export DJANGO_SUPERUSER_USERNAME=staff
+export DJANGO_SUPERUSER_EMAIL=test@email.com
+export DJANGO_SUPERUSER_PASSWORD=staffuserpassword
+python3 manage.py createsuperuser --no-input
 
 gunicorn nodata.wsgi:application --bind 0.0.0.0:8000
